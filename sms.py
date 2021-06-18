6b9b77d5c917ec95d9366165ac89fdeddb1694e1
Patrick Friedman <patrick.s.friedman@gmail.com>
2021-06-18 14:02:44 -0500

    Update sms.py

--- sms.py
+++ sms.py
@@ -1,13 +1,13 @@
 import os
 from twilio.rest import Client
 
-account_sid = 'AC20aef97ee2405b304d03111b5ae30439'
-auth_token = 'ae2682f547d1235d992c4bd32917f5aa'
+account_sid = 'AC20ae234405b304d03111b5ae30439'
+auth_token = 'ae2682f54756a4dfdsfasd5f48e255aa'
 client = Client(account_sid, auth_token)
 message = client.messages \
                 .create(
                      body="test",
-                     from_='+19725034157',
-                     to='+12144713159'
+                     from_='Twilio_num',
+                     to='Personal_num'
                  )
 print(message.sid)
